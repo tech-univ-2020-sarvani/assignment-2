83e94a13bc777927668684a1d4e8992be65b9946
sarvanideekshitula <sarvanimini@gmail.com>
2020-01-07 17:50:43 +0000

    Bash script

---
+++ assignment.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+echo "Enter your favourite colour"
+read COL1
+echo "Enter your friend's favourite colour"
+read COL2
+
+log=log_file.log
+printf "Log File- " > $log
+if [ "$COL1" == "$COL2" ]; then
+	echo "You both think alike" >> $log
+else
+	echo "I guess opposite attract" >> $log
+fi
