9d4f2d9a3e987c60231bc753ce9607535d9e381d
sarvanideekshitula <sarvanimini@gmail.com>
2020-01-07 17:56:08 +0000

    Modified return message

--- assignment.sh
+++ assignment.sh
@@ -7,7 +7,7 @@ read COL2
 log=log_file.log
 printf "Log File- " > $log
 if [ "$COL1" == "$COL2" ]; then
-	echo "You both think alike" >> $log
+	echo "You both think alike!!!" >> $log
 else
-	echo "I guess opposite attract" >> $log
+	echo "I guess opposite attract!!!" >> $log
 fi
