746ea4daad65e83822b3bddcc312fbe60c5df4ce
sarvanideekshitula <sarvanimini@gmail.com>
2020-01-07 19:31:51 +0000

    Bash script in master branch

--- assignment.sh
+++ assignment.sh
@@ -5,7 +5,7 @@ echo "Enter your friend's favourite colour"
 read COL2
 
 log=log_file.log
-printf "Log File- " > $log
+printf "Log File in master branch- " > $log
 if [ "$COL1" == "$COL2" ]; then
 	echo "You both think alike!!!" >> $log
 else
